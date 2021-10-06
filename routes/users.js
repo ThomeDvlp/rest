4c62d537afa637f08953dee7cf3638b03b378121
ThomeDvlp <thomedvlt@gmail.com>
2021-10-05 23:20:31 -0300

    Login JWT

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql').pool;
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 router.post('/registration/', (req, res, next) => {
     mysql.getConnection((err, conn) => {
@@ -33,4 +34,38 @@ router.post('/registration/', (req, res, next) => {
       }) 
   });
 })
+
+router.post('/login', (req, res, next) => {
+  mysql.getConnection((error, conn) =>{
+    if(error) { return res.status(500).send({error: error}) }
+    const query = `SELECT * FROM users WHERE email = ? `;
+    conn.query(query, [req.body.email], (error, results, fields) =>{
+      conn.release();
+      if (error) { return res.status(500).send({ error: error})}
+      if (results.length < 1) {
+        return res.status(401).send({mensagem: 'Falha na autenticação'})
+      }
+      bcrypt.compare(req.body.password, results[0].password, (err, result) => {
+        if(err) {
+          return res.status(401).send({mensagem: 'Falha na autenticação'})
+        }
+        if(result) {
+          const token = jwt.sign({
+            user_id: results[0].user_id,
+            email: results[0].email
+          }, process.env.JWT_KEY,
+          {
+            expiresIn: "1h"
+          });
+          return res.status(200).send({
+            mensagem:'Autenticado com sucesso',
+            token: token
+          })
+        }
+        return res.status(401).send({mensagem: 'Falha na autenticação'})
+      })
+    });
+  });
+})
+
 module.exports = router;
